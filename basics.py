3f2a8658ac86dd870dea3e23a0656b38794279cb
Shashank Kumar <shsharma93@gmail.com>
2019-05-04 21:01:43 +1000

    initial commit

---
+++ basics.py
@@ -0,0 +1,9 @@
+# Data type
+# Numbers - Integers = 10, -3, int('3') = 3, int(True) = 1
+# Numbers - Float = 1.8, -3.687, float('29') = 29.0, float(True) = 1.0
+# Boolean - True or False
+# Strings - 'Shashank', "Shashank", """Multiple line string inside three quote mark"""
+# Operators - +, -, *, /, //, **, %
+# List - Array in other language. ['Shashank', 9, True, ['nested', 9 ]] - length = 4
+# List - append() to add value at end in lists, pop() to remove last element from list. 
+
